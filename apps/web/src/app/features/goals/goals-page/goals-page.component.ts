25ade088bfb08b9ed69ae0abb859f1fafb2e7a9d
Zack Wotawa <zwotawa@gmail.com>
2026-04-14 06:04:42 -0500

    add error and loading state to goals page; modify all to be observalbe data

--- apps/web/src/app/features/goals/goals-page/goals-page.component.ts
+++ apps/web/src/app/features/goals/goals-page/goals-page.component.ts
@@ -1,44 +1,122 @@
-import { Component, OnInit } from '@angular/core';
-import { SEED_GOALS } from 'src/app/core/data/seed-goals';
+import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
+import { catchError, map, shareReplay, startWith } from 'rxjs/operators';
+
 import { Goal } from 'src/app/core/models/goal.model';
 import { GoalStoreService } from 'src/app/core/services/goal-store.service';
+
+interface Loadable<T> {
+  loading: boolean;
+  data: T | null;
+  error: string | null;
+}
+
+interface GoalsPageViewModel {
+  goalsState: Loadable<Goal[]>;
+  goals: Goal[];
+  filteredGoals: Goal[];
+  filteredGoalCount: number;
+  statusFilter: string;
+  laneFilter: string;
+  typeFilter: string;
+  pageLoading: boolean;
+  pageErrorMessages: string[];
+}
 
 @Component({
   selector: 'app-goals-page',
   templateUrl: './goals-page.component.html',
   styleUrls: ['./goals-page.component.scss']
 })
-export class GoalsPageComponent implements OnInit {
+export class GoalsPageComponent {
+  private readonly statusFilterSubject = new BehaviorSubject<string>('');
+  private readonly laneFilterSubject = new BehaviorSubject<string>('');
+  private readonly typeFilterSubject = new BehaviorSubject<string>('');
 
-  public goals: Goal[] = [];
-  public statusFilter: string = '';
-  public laneFilter: string = '';
-  public typeFilter: string = '';
+  public readonly statusFilter$ = this.statusFilterSubject.asObservable();
+  public readonly laneFilter$ = this.laneFilterSubject.asObservable();
+  public readonly typeFilter$ = this.typeFilterSubject.asObservable();
 
-  constructor(private goalStoreService: GoalStoreService) { }
+  private readonly goalsState$: Observable<Loadable<Goal[]>> =
+    this.goalStoreService.getGoals().pipe(
+      map(goals => ({
+        loading: false,
+        data: goals,
+        error: null
+      })),
+      startWith({
+        loading: true,
+        data: null,
+        error: null
+      }),
+      catchError(() =>
+        of({
+          loading: false,
+          data: null,
+          error: 'Could not load goals.'
+        })
+      ),
+      shareReplay(1)
+    );
 
-  ngOnInit(): void {
-    //this.goalStoreService.saveGoals(SEED_GOALS);
-    this.extractGoals();
+  public readonly vm$: Observable<GoalsPageViewModel> = combineLatest([
+    this.goalsState$,
+    this.statusFilter$,
+    this.laneFilter$,
+    this.typeFilter$
+  ]).pipe(
+    map(([goalsState, statusFilter, laneFilter, typeFilter]) => {
+      const goals = goalsState.data ?? [];
+      const filteredGoals = goals.filter(goal => {
+        return (
+          (statusFilter ? goal.status === statusFilter : true) &&
+          (laneFilter ? goal.lane === laneFilter : true) &&
+          (typeFilter ? goal.type === typeFilter : true)
+        );
+      });
+
+      const pageErrorMessages = [
+        goalsState.error
+      ].filter((message): message is string => !!message);
+
+      return {
+        goalsState,
+        goals,
+        filteredGoals,
+        filteredGoalCount: filteredGoals.length,
+        statusFilter,
+        laneFilter,
+        typeFilter,
+        pageLoading: goalsState.loading,
+        pageErrorMessages
+      };
+    }),
+    shareReplay(1)
+  );
+
+  constructor(
+    private readonly goalStoreService: GoalStoreService
+  ) {}
+
+  public updateStatusFilter(value: string): void {
+    this.statusFilterSubject.next(value);
   }
 
-   private extractGoals(): void {
-    this.goalStoreService.getGoals().subscribe({
-      next: (goals) => this.goals = goals
-    })
+  public updateLaneFilter(value: string): void {
+    this.laneFilterSubject.next(value);
   }
 
-  get filteredGoals(): Goal[] {
-    return this.goals.filter(goal => {
-      return (
-        (this.statusFilter ? goal.status === this.statusFilter : true) &&
-        (this.laneFilter ? goal.lane === this.laneFilter : true) &&
-        (this.typeFilter ? goal.type === this.typeFilter : true)
-      );
-    });
+  public updateTypeFilter(value: string): void {
+    this.typeFilterSubject.next(value);
+  }
+
+  public clearFilters(): void {
+    this.statusFilterSubject.next('');
+    this.laneFilterSubject.next('');
+    this.typeFilterSubject.next('');
   }
 
   public trackByGoalId(index: number, goal: Goal): string {
-  return goal.id;
-}
+    return goal.id;
+  }
 }
